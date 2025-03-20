1ecb48cd525f90dcd4d1ef0a844dcd97d61601bd
Aaron Bycoffe <bycoffe@gmail.com>
2025-03-20 15:55:39 -0400

    Add email on portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,4 +1,5 @@
 import { getPortfolioData } from '../../components/PortfolioData';
+import SocialLink from '../../components/SocialLink'
 import PortfolioItem from '../../components/PortfolioItem';
 
 export const metadata = {
@@ -16,8 +17,13 @@ export default async function PortfolioPage() {
         <h1 className="text-4xl md:text-5xl font-bold mb-3 bg-clip-text text-transparent bg-gradient-to-r from-gray-800 to-gray-600">
           Aaron Bycoffe
         </h1>
-        <p className="text-xl text-gray-600 max-w-2xl mx-auto">Portfolio</p>
+        <p className="text-xl text-gray-600 max-w-2xl mx-auto mb-5">Portfolio</p>
+        <SocialLink href="mailto:bycoffe@gmail.com">
+          bycoffe@gmail.com
+        </SocialLink>
       </header>
+      <div>
+      </div>
 
       <main className="w-full max-w-[1200px] px-4">
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-10 xl:gap-12">
